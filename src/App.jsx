ede2fbc6018bc3b02eec72c41dfad71a7852ae6b
poncho zazueta <alfonsozazuetab@gmail.com>
2025-03-10 20:22:12 -0600

    V2 of eCommerce store, with backend fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./components/Home/HomePage";
-import ProductsPage from "./components/Products/ProductsPage";
-import SingleProductPage from "./components/SingleProduct/SingleProductPage";
-import CartPage from "./components/Cart/CartPage";
-import MyOrder from "./components/MyOrder/MyOrder";
-import LoginPage from "./components/Authentication/LoginPage";
-import SignupPage from "./components/Authentication/SignupPage";
+import Routing from "./components/Routing/Routing.jsx";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <main>
-        {/* <HomePage /> */}
-        {/* <ProductsPage /> */}
-        {/* <SingleProductPage /> */}
-        {/* <CartPage /> */}
-        {/* <MyOrder /> */}
-        {/* <LoginPage /> */}
-        <SignupPage />
+        <Routing />
       </main>
     </div>
   );
--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+
 import ProductCard from "./ProductCard";
 import "./ProductsList.css";
+import useData from "../../hooks/UseData";
+import ProductCardSkeleton from "./ProductCardSkeleton";
+import { useSearchParams } from "react-router-dom";
+import Pagination from "../Common/Pagination";
 
 const ProductsList = () => {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useSearchParams();
+  const category = search.get("category");
+
+  const { data, error, isLoading } = useData(
+    "/products",
+    {
+      params: {
+        category,
+        perPage: 10,
+        page,
+      },
+    },
+    [category, page]
+  );
+
+  useEffect(() => {
+    setPage(1);
+  }, [category]);
+
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const handlePageChange = (page) => {
+    const currentParams = Object.fromEntries([...search]);
+    console.log(currentParams);
+
+    setSearch({ ...currentParams, page: parseInt(currentParams.page) + 1 });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+      if (
+        scrollTop + clientHeight >= scrollHeight - 1 &&
+        !isLoading &&
+        data &&
+        page < data.totalPages
+      ) {
+        console.log("reached bottom");
+        setPage((prev) => prev + 1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [data, isLoading]);
+
   return (
     <section className="products_list_section">
       <header className="align_center products_list_header">
@@ -17,15 +71,30 @@ const ProductsList = () => {
       </header>
 
       <div className="products_list">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {error && <em className="form_error">{error}</em>}
+        {data?.products &&
+          data.products.map((product) => (
+            <ProductCard
+              key={product._id}
+              id={product._id}
+              image={product.images[0]}
+              price={product.price}
+              title={product.title}
+              rating={product.reviews.rate}
+              ratingCounts={product.reviews.counts}
+              stock={product.stock}
+            />
+          ))}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
       </div>
+      {/* {data && (
+        <Pagination
+          totalPosts={data?.totalProducts}
+          postsPerPage={8}
+          onClick={handlePageChange}
+          currentPage={page}
+        />
+      )} */}
     </section>
   );
 };
